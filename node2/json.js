1baca543ff48d01203e0384f3d44fb1acdab9794
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-16 18:54:48 +0900

    json

---
+++ node2/json.js
@@ -0,0 +1,12 @@
+const user = {};
+user.name = 'kyeongrok';
+user.age = 31;
+
+console.log(user);
+
+const user2 = {
+    name:'kyeongrok',
+    age: 31
+};//해당하는 값에 원래는 ""를 붙여주어야 하지만 코딩 편의상 붙혀주지 않아도 됨
+
+console.log(user2);
