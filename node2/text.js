f23dd77bd72222bd30b8386226d5c537c2010065
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-16 19:20:56 +0900

    process

---
+++ node2/text.js
@@ -0,0 +1,12 @@
+console.log(process.env);//컴퓨터 환경 관련 정보
+console.log('-----------------------------');
+console.log(process.arch);//프로세서의 아키텍쳐
+console.log('-----------------------------');
+console.log(process.uptime());//현재 프로그램이 실행된 시간
+console.log('-----------------------------');
+console.log(process.memoryUsage());//메모리 사용 정보를 가진 객체
+console.log('-----------------------------');
+console.log(process.version);//node js 버전
+console.log('-----------------------------');
+console.log(process.versions);//node js 버전
+
