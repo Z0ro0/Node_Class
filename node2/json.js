0c4eafcf2d2d576bcd2ddb728c9f077e53c481b2
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-16 18:59:19 +0900

    json 객체에서 값 추출 시 [] 또는 .연산자 이용

--- node2/json.js
+++ node2/json.js
@@ -10,3 +10,18 @@ const user2 = {
 };//해당하는 값에 원래는 ""를 붙여주어야 하지만 코딩 편의상 붙혀주지 않아도 됨
 
 console.log(user2);
+
+const user3 = {
+    name: 'kim',
+    age: 30
+};
+console.log(user3);
+console.log(user3.name);
+console.log(user3.age);
+
+user3.job = 'developer';
+user3.nation = 'korea';
+console.log(user3);
+
+const fieldName = 'age';
+console.log(user3[fieldName]);
