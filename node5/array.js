9271cc52a401fb0a285edf2eaea23fc21ba9358b
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-28 18:48:04 +0900

    .pop()

---
+++ node5/array.js
@@ -0,0 +1,14 @@
+let array = [{
+    name: '고구마',
+    price: 1000
+},{
+    name: '감자',
+    price:500
+},{
+    name: '바나나',
+    price: 1500
+}];
+
+let popped = array.pop();//배열 요소 꺼내기
+console.log(popped);
+console.log(array);
