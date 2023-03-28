cc07f71f0eb9e0c8e308492f18976e7a6a23d8a4
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-28 19:54:14 +0900

    웹 서버 시작 접속 요청 종료

---
+++ node5/requireEx.js
@@ -0,0 +1,30 @@
+var http = require('http');
+
+var server = http.createServer();
+
+var port = 3000;
+server.listen(port, function () {
+  console.log("웹 서버가 시작되었습니다. : %d", port);
+});
+//클라이언트 연결 이벤트 처리
+//이벤트는 on으로 처리 connection이 발생하면 내부적으로 socket 생성
+server.on("connection", function (socket) {
+  console.log(
+    "클라이언트가 접속했습니다 : %s, %d",
+    socket.remoteAddress,
+    socket.remotePort
+  );
+});
+
+//클라이언트의 요청(reuest) 이벤트 처리
+server.on("request", function (req, res) {
+  console.log("클라이언트 요청이 들어왔습니다.");
+  console.dir(req);
+});
+
+//서버 종료 이벤트 처리
+server.on("close", function () {
+  console.log("서버가 종료됩니다.");
+});
+
+//ctrl c로 서버 나가야 3000에서 실행할 수 있음
