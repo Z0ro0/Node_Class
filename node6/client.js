e408458036375bef054f5d6160d750abb90db14f
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-30 19:36:42 +0900

    주석 설명

--- node6/client.js
+++ node6/client.js
@@ -1,4 +1,8 @@
+//request 이벤트 처리할 때 writeHead(), write(), end() 메소드로 응답 전송
+
+//웹 서버를 생성하고 실행
 require('http').createServer(function(request, response){
+    //응답합니다
     response.writeHead(200,{'Content-Type':'text/html'});
     response.end('<h1>Hello Web Server with Node js</h1>');
 }).listen(52273, function(){
